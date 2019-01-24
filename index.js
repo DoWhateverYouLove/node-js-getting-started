f1e704d6204af895347087df8990cbe16ab1f753
caiweijian <1793253408@qq.com>
2019-01-24 09:33:09 +0800

    new a app

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const app = express()
+const PORT = 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'dist')))
+app.listen(PORT, () => {
+    console.log('app listening on port 5000.')
+})
